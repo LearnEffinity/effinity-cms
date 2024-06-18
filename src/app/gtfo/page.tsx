f1496a2e5d163f6bef7cf4e14d808cf818974601
Aditya Sahasranam <adityasahas2025@gmail.com>
2024-06-18 12:35:44 -0700

    chore: Refactor GTFO page to use Supabase client for sign out functionality

--- src/app/gtfo/page.tsx
+++ src/app/gtfo/page.tsx
@@ -1,17 +1,19 @@
 "use client";
-
+import { createClient } from "@/utils/supabase/client";
+import Button from "@/components/form/Button";
 
 export default function Home() {
+  const supabase = createClient();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono lg:flex">
         <h1 className="text-[50px]">You don't have admin! GTFO!</h1>
-        <a
+        <Button
           className="rounded-lg bg-brand-primary px-8 py-4 text-center text-white"
-          href="/signout"
+          onClick={() => supabase.auth.signOut()}
         >
           Sign out
-        </a>
+        </Button>
         <video src="/monkey.mp4" autoPlay loop />
       </div>
     </main>
